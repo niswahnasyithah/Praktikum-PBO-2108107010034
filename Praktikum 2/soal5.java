2baa1686a7bf11532e3365560b6b339da81cc0fb
Niswah <107196009+niswahnasyithah@users.noreply.github.com>
2022-09-11 14:18:53 +0700

    Update soal5.java

--- Praktikum 2/soal5.java
+++ Praktikum 2/soal5.java
@@ -1,4 +1,4 @@
-/*program menampilkan bilangan genap dan bukan kelipatan 6 dari 1-20  */
+/*program menampilkan bilangan genap yang bukan kelipatan 6 dari 1-20  */
 public class soal5 
 {
   public static void main(String args[]){
