fef7e93009912e7292971ea960b15514cd47eeef
Niswah <107196009+niswahnasyithah@users.noreply.github.com>
2022-09-11 14:04:23 +0700

    Add files via upload

---
+++ Praktikum 2/soal4.java
@@ -0,0 +1,12 @@
+/*program menampilkan deret bilagan genap dari 1-20  */
+public class soal4 
+{
+  public static void main(String args[]){
+    System.out.println("Deret Bilangan Genap");
+        for(int i=1;i<=20;i++){
+            if (i%2==0)
+            System.out.print(i+" ");
+    
+        }
+  }
+}
